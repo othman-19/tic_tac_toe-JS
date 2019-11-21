0e0f2847a5cbef77a5e379c4f5fd7e21bc25d3b9
Sumaer Jolly <sumaerjolly@gmail.com>
2019-11-21 14:55:04 +0530

    refactored code

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-// // Player Class
-// let origBoard;
-// const winCombos = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [6, 4, 2]
-// ];
 let boardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 const playerFactory = (name, selection) => {
   return { name, selection };
 };
@@ -37,6 +26,7 @@ let player2 = (function() {
   player1.selection == 'X'
     ? (player2selection = 'O')
     : (player2selection = 'X');
+  alert(`${player2name} you are ${player2selection}`);
   return playerFactory(player2name, player2selection);
 })();
 let currentSelection = player1.selection;
@@ -55,15 +45,14 @@ function removeEventListenerFromCell() {
 }
 
 function fillCell(e) {
-  //console.log(e.target.innerText)
   if (!e.target.innerText) {
     e.target.innerText = currentSelection;
     putInBoard(e.target.id, currentSelection);
     if (checkWinner(currentSelection)) {
       endGame();
-    }else {
-      if(checkTie()){
-        tieDisplay()
+    } else {
+      if (checkTie()) {
+        tieDisplay();
         endGame();
       }
     }
@@ -86,7 +75,6 @@ function checkWinner(l) {
     (boardArray[0] == l && boardArray[4] == l && boardArray[8] == l) ||
     (boardArray[6] == l && boardArray[4] == l && boardArray[2] == l)
   ) {
-    console.log(declareWinner(l));
     return declareWinner(l);
   } else {
     return false;
@@ -112,15 +100,16 @@ function winnerDisplay(winner) {
   resultText.innerHTML = `The winner is ${winner}`;
 }
 
-function checkTie(){
-  return boardArray.every( i => { return typeof i === "string" });
+function checkTie() {
+  return boardArray.every(i => {
+    return typeof i === 'string';
+  });
 }
 
 function tieDisplay() {
   resultDisplay.style.display = 'block';
   resultText.innerHTML = `The game is a draw`;
 }
-
 
 function endGame() {
   removeEventListenerFromCell();
@@ -142,103 +131,10 @@ function start() {
 function reset() {
   for (let i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
-    // cells[i].style.removeProperty('background-color');
   }
   start();
 }
 
-const resetBtn = document.querySelector('BUTTON')
-resetBtn.addEventListener("click", reset)
+const resetBtn = document.querySelector('BUTTON');
+resetBtn.addEventListener('click', reset);
 start();
-
-// // player setting ends here
-
-// let origBoard;
-// const huPlayer = 'O';
-// const aiPlayer = 'X';
-// const winCombos = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [6, 4, 2]
-// ];
-
-// const cells = document.querySelectorAll('.cell');
-// startGame();
-
-// function startGame() {
-//   document.querySelector('.endgame').style.display = 'none';
-//   origBoard = Array.from(Array(9).keys());
-//   for (let i = 0; i < cells.length; i++) {
-//     cells[i].innerText = '';
-//     cells[i].style.removeProperty('background-color');
-//     cells[i].addEventListener('click', turnClick, false);
-//   }
-// }
-
-// function turnClick(square) {
-//   if (typeof origBoard[square.target.id] == 'number') {
-//     turn(square.target.id, huPlayer);
-//     if (!checkTie()) turn(bestSpot(), aiPlayer);
-//   }
-// }
-
-// function turn(squareId, player) {
-//   origBoard[squareId] = player;
-//   document.getElementById(squareId).innerText = player;
-//   let gameWon = checkWin(origBoard, player);
-//   if (gameWon) gameOver(gameWon);
-// }
-
-// function checkWin(board, player) {
-//   let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
-//   let gameWon = null;
-//   for (let [index, win] of winCombos.entries()) {
-//     if (win.every(elem => plays.indexOf(elem) > -1)) {
-//       gameWon = { index: index, player: player };
-//       break;
-//     }
-//   }
-//   return gameWon;
-// }
-
-// function gameOver(gameWon) {
-//   for (let index of winCombos[gameWon.index]) {
-//     document.getElementById(index).style.backgroundColor =
-//       gameWon.player == huPlayer ? 'blue' : 'red';
-//   }
-
-//   for (let i = 0; i < cells.length; i++) {
-//     cells[i].removeEventListener('click', turnClick, false);
-//   }
-//   declareWinner(gameWon.player == huPlayer ? 'You Win' : 'You Lose');
-// }
-
-// function declareWinner(who) {
-//   document.querySelector('.endgame').style.display = 'block';
-//   document.querySelector('.endgame .text').innerText = who;
-// }
-
-// function emptySquares() {
-//   return origBoard.filter(s => typeof s == 'number');
-// }
-
-// function bestSpot() {
-//   return emptySquares()[0];
-// }
-
-// function checkTie() {
-//   if (emptySquares().length == 0) {
-//     for (let i = 0; i < cells.length; i++) {
-//       cells[i].style.backgroundColor = 'green';
-//       cells[i].removeEventListener('click', turnClick, false);
-//     }
-//     declareWinner('Tie Game');
-//     return true;
-//   }
-//   return false;
-// }
