36b806e59c81aababae4c53f296ad6787e8d9aca
Sumaer Jolly <sumaerjolly@gmail.com>
2019-11-21 12:04:05 +0530

    reset function added

--- script.js
+++ script.js
@@ -11,62 +11,70 @@
 //   [6, 4, 2]
 // ];
 
+const playerFactory = (name, selection) => {
+  return { name, selection };
+};
 
-function start(){
-
-  const playerFactory = (name, selection) => {
-    return { name, selection };
-  };
-  
-  let player1 = (function () {
-    let player1name = prompt('Player 1 please enter your name');
-    let player1selection = prompt(
+let player1 = (function() {
+  let player1name = prompt('Player 1 please enter your name');
+  let player1selection = prompt(
+    ` ${player1name} please select X or O`
+  ).toUpperCase();
+  while (
+    player1selection.toUpperCase() != 'X' &&
+    player1selection.toUpperCase() != 'O'
+  ) {
+    player1selection = prompt(
       ` ${player1name} please select X or O`
     ).toUpperCase();
-    while (
-      player1selection.toUpperCase() != 'X' &&
-      player1selection.toUpperCase() != 'O'
-    ) {
-      player1selection = prompt(
-        ` ${player1name} please select X or O`
-      ).toUpperCase();
-    }
-    return playerFactory(player1name, player1selection)
-  })();
-  
-  let player2 = (function(){
-    let player2name = prompt('Player 2 please enter your name');
-    let player2selection;
-    player1.selection == 'X' ? (player2selection = 'O') : (player2selection = 'X');
-    return playerFactory(player2name, player2selection);
-  })();
-
-  let currentSelection = player1.selection;
-  const cells = document.querySelectorAll('.cell');
-
-  function addEventListenerToCell(){
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].addEventListener('click', fillCell, false);
-    }
-  } 
-  addEventListenerToCell();
-  function fillCell(e){
-    //console.log(e.target.innerText)
-    if (!e.target.innerText){
-      e.target.innerText = currentSelection
-      changeRole()
-    }
   }
+  return playerFactory(player1name, player1selection);
+})();
 
-  function changeRole(){
-    currentSelection == player1.selection? currentSelection = player2.selection : currentSelection = player1.selection;
+let player2 = (function() {
+  let player2name = prompt('Player 2 please enter your name');
+  let player2selection;
+  player1.selection == 'X'
+    ? (player2selection = 'O')
+    : (player2selection = 'X');
+  return playerFactory(player2name, player2selection);
+})();
+let currentSelection = player1.selection;
+const cells = document.querySelectorAll('.cell');
+
+function addEventListenerToCell() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', fillCell, false);
   }
 }
+
+function fillCell(e) {
+  //console.log(e.target.innerText)
+  if (!e.target.innerText) {
+    e.target.innerText = currentSelection;
+    changeRole();
+  }
+}
+
+function changeRole() {
+  currentSelection == player1.selection
+    ? (currentSelection = player2.selection)
+    : (currentSelection = player1.selection);
+}
+
+function start() {
+  addEventListenerToCell();
+}
+
+function reset() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    // cells[i].style.removeProperty('background-color');
+  }
+  start();
+}
+
 start();
-
-
-
-
 
 // // player setting ends here
 
